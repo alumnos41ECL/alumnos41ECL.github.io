fc5972087aaa5fe53e048294215a9b12ac342ec8
alumnos41ECL <81402336+alumnos41ECL@users.noreply.github.com>
2021-03-26 00:12:20 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCQ_5ialfH2-J2MfNkwisGCJNpPRJms65I",
+    authDomain: "alumnos41ecl-e0c8a.firebaseapp.com",
+    projectId: "alumnos41ecl-e0c8a",
+    storageBucket: "alumnos41ecl-e0c8a.appspot.com",
+    messagingSenderId: "803052170814",
+    appId: "1:803052170814:web:5882e60c60c6c358592b6f",
+    measurementId: "G-Z0KJVTYP6N"
+    });
